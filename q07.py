948b658ff0b53875600a1384d77a638e65d38605
GitCida <mariaaparecidaily2@gmail.com>
2024-06-07 21:44:32 -0300

    revisado e alterado

--- q07.py
+++ q07.py
@@ -1,11 +1,10 @@
 nome = input("Digite seu nome: ")
-disc = input("Digite o nome da matéria que deseja saber a média: ")
-n1 = int(input("Diga sua primeira nota: "))
-n2 = int(input("Diga sua segunda nota: "))
+disciplina = input("Digite o nome da matéria que deseja saber a média: ")
+n1 = float(input("Diga sua primeira nota: "))
+n2 = float(input("Diga sua segunda nota: "))
 m = (n1+n2)/2
-#de 0 a 5: reprovado / de 6 a 10: aprovado 
-if (m > 0 and m < 6 ):
-    print(f'{nome} está reprovado(a) na disciplina de {disc}.')
+if m <=5:
+    situação = 'reprovado'
 else:
-    print(f'{nome} está aprovado(a) na disciplina de {disc}.')
-#Não precisaria dizer a nota?
+    situação = 'aprovado'
+print(f'{nome} está {situação} na disciplina {disciplina}.')
