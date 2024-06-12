0a4b697b9108aa189e9a7d35922a3cdba85c8cff
GitCida <mariaaparecidaily2@gmail.com>
2024-06-12 17:58:45 -0300

    alterado e revisado

--- q02.py
+++ q02.py
@@ -1,7 +1,7 @@
 var_int = 1289
 var_float = 0.15
 var_str = "Trabalho De Python e Github"
-var_bool = 6 == 6 
+var_bool = 6 == 6
 print(f'{var_int} é da {type(var_int)}')
 print(f'{var_float} é da {type(var_float)}')
 print(f'{var_str} é da {type(var_str)}')
--- q07.py
+++ q07.py
@@ -3,8 +3,8 @@ disciplina = input("Digite o nome da matéria que deseja saber a média: ")
 n1 = float(input("Diga sua primeira nota: "))
 n2 = float(input("Diga sua segunda nota: "))
 m = (n1+n2)/2
-if m <=5:
-    situação = 'reprovado'
+if m <6:
+    situação = 'reprovado(a)'
 else:
-    situação = 'aprovado'
+    situação = 'aprovado(a)'
 print(f'{nome} está {situação} na disciplina {disciplina}.')
